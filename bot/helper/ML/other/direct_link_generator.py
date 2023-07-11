7ad048b9f30d0a1b66c2b2a8755b6cfcb24ead7b
sisakoy <137015779+sisakoy@users.noreply.github.com>
2023-07-11 18:12:19 +0000

    Update direct_link_generator.py

--- bot/helper/ML/other/direct_link_generator.py
+++ bot/helper/ML/other/direct_link_generator.py
@@ -509,10 +509,10 @@ def terabox(url) -> str:
         raise DirectDownloadLinkException("ERROR: terabox.txt not found")
     session = create_scraper()
     try:
+        key = url.split('?surl=')[-1]
+        url = f'http://www.terabox.com/wap/share/filelist?surl={key}'
         jar = MozillaCookieJar('terabox.txt')
         jar.load()
-        cookie_string = ''
-        for cookie in jar: cookie_string += f'{cookie.name}={cookie.value}; '
         session.cookies.update(jar)
         res = session.request('GET', url)
         key = res.url.split('?surl=')[-1]
@@ -522,9 +522,8 @@ def terabox(url) -> str:
             fstring = fs.string
             if fstring and fstring.startswith('try {eval(decodeURIComponent'):
                 jsToken = fstring.split('%22')[1]
-        headers = {"Cookie": cookie_string}
         res = session.request(
-            'GET', f'https://www.terabox.com/share/list?app_id=250528&jsToken={jsToken}&shorturl={key}&root=1', headers=headers)
+            'GET', f'https://www.terabox.com/share/list?app_id=250528&jsToken={jsToken}&shorturl={key}&root=1')
         result = res.json()
     except Exception as e:
         raise DirectDownloadLinkException(f"ERROR: {e.__class__.__name__}")
